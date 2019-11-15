7cd25f142b33c0ee81456826f7346eeb4fb6357b
Eduardo Fleck <eduardo.fleck@nn.cz>
2019-11-15 13:35:35 +0100

    Removed firebase`s API`s references

--- src/base.js
+++ src/base.js
@@ -2,10 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBzeQm9ehvhuu-Iq1VNEyw6DO0pEmjVesM",
-    authDomain: "petra-karaoke.firebaseapp.com",
-    databaseURL: "https://petra-karaoke.firebaseio.com"
-
+    apiKey: process.env.REACT_APP_PATRA_KARAOKE_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_PATRA_KARAOKE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_PATRA_KARAOKE_FIREBASE_DATABASE_URL
 });
 
 const base = Rebase.createClass(firebaseApp.database());
